6abc1e6a01c281ec24bb3e9ae69d7e4036d5ce7a
DeekshithaMariyappan <deekshithamariyappan@gmail.com>
2026-06-15 22:20:11 +0530

    Added Question 5

---
+++ question5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class question5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        System.out.println("Binary number: " + Integer.toBinaryString(num));
+    }
+}

f52725e413d4dd67eab5c1264cb96ecc6a3ea7ba
DeekshithaMariyappan <deekshithamariyappan@gmail.com>
2026-06-15 22:15:25 +0530

    Added Question 3

---
+++ question3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class question3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        double area = Math.PI * radius * radius;
+        double perimeter = 2 * Math.PI * radius;
+
+        System.out.println("Area of the circle: " + area);
+        System.out.println("Perimeter of the circle: " + perimeter);
+
+        sc.close();
+    }
+} 
+    
+
